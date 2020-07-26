7d28b3ab77a2deff236b3b81251db1c83a7abee3
Bum June Kim <bjbj6363@gmail.com>
2020-07-26 17:17:28 +0900

    implement usage function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    usage();
+}
+
+fn usage() {
+    println!("tinymd, a markdown compiler written by Jonir227");
+    println!("Version: {}", get_version());
+}
+
+fn get_version() -> u16 {
+    1
 }
