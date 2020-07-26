4684a0805fa6760ba60a02f7da175fac7d1ba4aa
Bum June Kim <bjbj6363@gmail.com>
2020-07-26 17:18:26 +0900

    add variable

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ fn main() {
 }
 
 fn usage() {
+    let version = get_version();
     println!("tinymd, a markdown compiler written by Jonir227");
-    println!("Version: {}", get_version());
+    println!("Version: {}", version);
 }
 
 fn get_version() -> u16 {
