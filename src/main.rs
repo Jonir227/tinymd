9c6c4047514256b3ff5f80a74ac4fd4e294d95c2
Bum June Kim <bjbj6363@gmail.com>
2020-07-26 17:40:21 +0900

    add feature the accept argments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    usage();
+    let args: Vec<String> = std::env::args().collect();
+    match args.len() {
+        2 => {
+            parse_markdown_file();
+        }
+        _ => {
+            println!("[ ERROR ] Invalid invocation (you done goofed!)");
+            usage();
+        }
+    }
 }
 
 fn usage() {
