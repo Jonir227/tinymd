378e70a276f86d43f9a08bd1f07857a12e8435bb
Bum June Kim <bjbj6363@gmail.com>
2020-07-26 17:36:35 +0900

    add function that print banner

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,29 @@ fn main() {
 }
 
 fn usage() {
-    let the_version: &str = "0.1";
-    println!("tinymd, a markdown compiler written by Jonir227");
-    println!("Version: {}", the_version);
+    print_long_banner();
+}
+
+fn parse_markdown_file() {}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    println!(
+        "Written by: {}\nHomepage: {}\nUsage: tinymd <somefile>.md\n",
+        env!("CARGO_PKG_AUTHORS"),
+        env!("CARGO_PKG_HOMEPAGE")
+    );
+}
+
+fn get_title() -> String {
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str("(v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("), ");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    return the_title;
 }
